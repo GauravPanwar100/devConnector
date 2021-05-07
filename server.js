d781fd809a1e90ec249146589631bea862633e62
gaurav panwar <gaurav.panwar@squareyards.co.in>
2021-05-07 10:26:51 +0530

    User registered successfully in mongodb

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const app = express();
 // Connect database
 connectDB();
 
+// Init Middleware
+app.use(express.json({ extended: false })); // this will allow us to get data in req.body
+
 app.get('/', (req, res) => res.send('API Running'));
 
 
