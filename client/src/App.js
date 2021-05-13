378852cee8f62c39526f164720ba99782a3feddb
gaurav panwar <gaurav.panwar@squareyards.co.in>
2021-05-14 02:05:32 +0530

    bind navbar and landing component successfully

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,14 @@
+import React, { Fragment } from 'react';
 import './App.css';
+import Navbar from './components/layout/Navbar';
+import Landing from './components/layout/Landing';
 
-function App() {
+const App = () => {
   return (
-    <div>gaurav</div>
+    <Fragment>
+      <Navbar />
+      <Landing />
+    </Fragment>
   );
 }
 
